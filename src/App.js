2657a1e3b10292fdfa730f7805f3bbc8c4a4d1fc
Trojanonloose <shreyasthakare5@gmail.com>
2023-03-21 14:57:22 +0530

    URL issue fix 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => (
         <Box sx={{ backgroundColor: '#000' }}>
             <Navbar />
             <Routes>
-                <Route exact path='/Youtube-clone' element={<Feed />} />
+                <Route exact path='/youtubeclone' element={<Feed />} />
                 <Route path='/video/:id' element={<VideoDetail />} />
                 <Route path='/channel/:id' element={<ChannelDetail />} />
                 <Route path='/search/:searchTerm' element={<SearchFeed />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
             p={2}
             sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}
         >
-            <Link to="/Youtube-clone" style={{ display: 'flex', alignItems: 'center' }}>
+            <Link to="/youtubeclone" style={{ display: 'flex', alignItems: 'center' }}>
                 <img src={logo} alt='logo' height={45} />
             </Link>
             <SearchBar />
